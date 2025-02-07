593187fff91d716847ebb8c0a2321a15c61847d4
Jamoliddin <jamoliddin0968@gmail.com>
2025-02-07 14:26:04 +0500

    change any templates

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Категория номи")
+    image = models.ImageField(upload_to='categories/', blank=True, null=True, verbose_name="Фон расм")
+    description = models.TextField(blank=True, null=True, verbose_name="Тавсиф")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Яратилган сана")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категориялар"
+        ordering = ['name']
+
+    
--- apps/home/views.py
+++ apps/home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.views.generic import TemplateView
 
+from apps.home.models import Category
+
 class HomeTemplateView(TemplateView):
     template_name = 'index.html'
 
@@ -10,6 +12,13 @@ class AboutTemplateView(TemplateView):
 
 class ProductTemplateView(TemplateView):
     template_name = 'products.html'
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        categories = list(Category.objects.all())  # Convert queryset to list
+        grouped_categories = [categories[i:i + 3] for i in range(0, len(categories), 3)]
+        context["grouped_categories"] = grouped_categories
+        return context
+    
 
 class ProdctionTemplateView(TemplateView):
     template_name = 'production.html'
