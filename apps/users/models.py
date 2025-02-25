2230d3455c3ef5a3987eb838a0a639705e175daf
Jamoliddin <jamoliddin0968@gmail.com>
2025-02-25 11:38:40 +0500

    add verobse names

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,12 +4,21 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    birth_date = models.DateField(null=True, blank=True)
-    position = models.CharField(max_length=100, null=True, blank=True)
-    address = models.CharField(max_length=255, null=True, blank=True)
-    work_time = models.TimeField(null=True,)
-    employee_id = models.CharField(max_length=20, null=True, blank=True)
-    phone = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(
+        max_length=150, blank=True, verbose_name=("Ism"))
+    last_name = models.CharField(
+        max_length=150, blank=True, verbose_name=("Familiya"))
+    birth_date = models.DateField(
+        null=True, blank=True, verbose_name="Tug'ilgan sana")
+    position = models.CharField(
+        max_length=100, null=True, blank=True, verbose_name="Lavozim")
+    address = models.CharField(
+        max_length=255, null=True, blank=True, verbose_name="Manzil")
+    work_time = models.TimeField(null=True, verbose_name="Ish vaqti")
+    employee_id = models.CharField(
+        max_length=20, null=True, blank=True, verbose_name="Xodim ID")
+    phone = models.CharField(max_length=20, null=True,
+                             blank=True, verbose_name="Telefon raqami")
     photo = models.ImageField(
         upload_to="users/", null=True, blank=True, verbose_name="Rasm")
 
@@ -35,20 +44,23 @@ class ATTENDANCE_STATUS_CHOICES(models.TextChoices):
 
 class Attendance(models.Model):
     user = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE, related_name="attendances")
-    date = models.DateField()
-    work_time = models.TimeField(null=True)
-    arrival_time = models.TimeField(null=True, blank=True)
+        CustomUser, on_delete=models.CASCADE, related_name="attendances",
+        verbose_name="Xodim")
+    date = models.DateField(verbose_name="Sana")
+    work_time = models.TimeField(null=True, verbose_name="Ish vaqti")
+    arrival_time = models.TimeField(
+        null=True, blank=True, verbose_name="Kelish vaqti")
     late_minutes = models.PositiveIntegerField(
-        null=True, blank=True)
-    reason = models.TextField(null=True, blank=True)
-    serial_id = models.PositiveIntegerField(null=True)
+        null=True, blank=True, verbose_name="Kechikish daqiqasi")
+    reason = models.TextField(null=True, blank=True, verbose_name="Sabab")
+    serial_id = models.PositiveIntegerField(
+        null=True, verbose_name="Tartib raqami")
     status = models.CharField(max_length=20, choices=ATTENDANCE_STATUS_CHOICES.choices,
-                              default=ATTENDANCE_STATUS_CHOICES.ABSENT)
+                              default=ATTENDANCE_STATUS_CHOICES.ABSENT, verbose_name="Holat")
 
     class Meta:
         verbose_name = "Davomat"
-        verbose_name_plural = "Davomatlar"
+        verbose_name_plural = "Davomat"
 
     def __str__(self):
         return f"{self.user.first_name} - {self.date} - {self.arrival_time or 'Kelmagan'}"
