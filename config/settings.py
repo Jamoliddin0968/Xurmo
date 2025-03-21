19ab23bd5c82a68ae16ef79fbeadd7f14fe94c32
Jamoliddin <jamoliddin0968@gmail.com>
2025-03-21 13:46:15 +0500

    change database

--- config/settings.py
+++ config/settings.py
@@ -96,11 +96,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    "mysql": {
+    "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
